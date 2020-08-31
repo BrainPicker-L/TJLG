1bac2ffe7d47e8c3954917ceaa4696cf5f23b59c
liuzizhe_sx <1500202278@qq.com>
2020-08-31 08:56:24 +0800

    fixbug

--- InfoApi/urls.py
+++ InfoApi/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.info, name="info"),
-    path("get_start_school_day", views.get_start_school_day,name="get_start_school_day")
+    url("get_start_school_day", views.get_start_school_day,name="get_start_school_day"),
+    url("", views.info, name="info"),
+
 ]
